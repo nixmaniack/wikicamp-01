a5923d41b33446f7ec0f46e71dfc797b74b65818
Muneeb Shaikh <iammuneeb@gmail.com>
2011-11-06 01:19:26 +0530

    change templates path comment
    Added a comment to change templates path according
    to the project settings.

--- settings.py
+++ settings.py
@@ -107,6 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    
+    # Change it according to your project settings
     "/home/muneeb/projects/django/wikicamp/templates"
 )
 
